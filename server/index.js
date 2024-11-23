e00ef55c65b93508ca25d1f5c81265ea2453e28b
Ruman7860 <115347738+Ruman7860@users.noreply.github.com>
2024-11-23 17:26:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -105,7 +105,7 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 server.listen(port,() => {
-    console.log(`server is running on port ${port}`)
+    console.log(`Server is running on port ${port}`);
 })

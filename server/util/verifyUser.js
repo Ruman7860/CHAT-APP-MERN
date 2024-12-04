613c143ed977dd7e19c2612260bbb88616c1dcf7
Ruman7860 <115347738+Ruman7860@users.noreply.github.com>
2024-12-04 19:02:28 +0530

    Update verifyUser.js

--- server/util/verifyUser.js
+++ server/util/verifyUser.js
@@ -5,8 +5,8 @@ import jwt from 'jsonwebtoken';
 export const verifyUser = async (req, res, next) => {
   try {
     const access_token = req.cookies.access_token;
-    console.log("Cookies:", req.cookies); // Debug cookies
-    console.log("Access Token:", access_token); // Debug token
+    // console.log("Cookies:", req.cookies); // Debug cookies
+    // console.log("Access Token:", access_token); // Debug token
 
     if (!access_token) {
       return next(errorHandler(401, "Unauthorized - Token not provided"));
